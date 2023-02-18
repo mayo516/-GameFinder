91a25c00f631b791d9b43c284d82c4174289586c
eunha <dmsgk2323@naver.com>
2023-02-18 15:58:35 +0900

    add : 보기 컴포넌트 만들기

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -11,9 +11,21 @@ export default function Main() {
         text={"게임분류선택"}
         choiceText={["고티수상작", "게임 전체 중에서"]}
       />
-      <Question text={"게임분류선택"} choiceText={[]} />
-      <Question text={"게임분류선택"} choiceText={[]} />
-      <Question text={"게임분류선택"} choiceText={[]} />
+      <Question
+        text={"선호 하는 장르 (중복 선택 가능)"}
+        choiceText={[
+          "RPG",
+          "MMOPRG",
+          "액션",
+          "샌드박스",
+          "FPS",
+          "RTS",
+          "딱히 없음",
+        ]}
+      />
+      <Question text={"인생 게임이 있습니까?"} choiceText={["Y", "N"]} />
+      <Question text={"좋아하는 게임을 입력하세요"} choiceText={[]} />
+      <input />
     </main>
   );
 }
