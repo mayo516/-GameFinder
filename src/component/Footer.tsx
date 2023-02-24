14b37c770f171867dcce1d723fe4d86424e24749
eunha <dmsgk2323@naver.com>
2023-02-24 17:53:20 +0900

    add: chapGPT 요청 함수 구현

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,9 +1,10 @@
+"use client";
 import React from "react";
-
+import handleSubmit from "../lib/api_request/chatgpt";
 export default function Footer() {
   return (
     <>
-      <footer>푸터입니다</footer>
+      <footer onClick={handleSubmit}>푸터입니다</footer>
     </>
   );
 }
---
+++ src/lib/api_request/chatgpt.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  let prompt = "Hi!";
+  // Send a request to the server with the prompt
+  axios
+    .post("http://localhost:8080/chat", JSON.stringify({ prompt: "hi!!!" }), {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((res) => {
+      // Update the response state with the server's response
+      console.log(res);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+export default handleSubmit;
