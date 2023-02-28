f1fa71bd7ec8be5507ba2c98b553b267ec661b83
eunha <dmsgk2323@naver.com>
2023-02-28 09:18:47 +0900

    add : open graph protocol

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -8,6 +8,21 @@ export default function Head() {
         content="게임 취향을 반영해서 게임을 추천해주는 사이트. 여러분이 좋아할만한 게임을 찾아드립니다. "
       />
       <link rel="icon" href="/favicon.ico" />
+      <meta property="og:title" content="게임파인더: GAME FINDER " />
+      <meta property="og:type" content="컨텐츠 타입" />
+      <meta property="og:url" content="페이지 주소" />
+      <meta property="og:image" content="페이지 썸네일 이미지 주소" />
+      <meta
+        property="og:description"
+        content="게임 취향을 반영해서 게임을 추천해주는 사이트. 여러분이 좋아할만한 게임을 찾아드립니다."
+      />
+      <meta name="twitter:card" content="Summary Card" />
+      <meta name="twitter:title" content="게임파인더: GAME FINDER" />
+      <meta
+        name="twitter:description"
+        content="게임 취향을 반영해서 게임을 추천해주는 사이트. 여러분이 좋아할만한 게임을 찾아드립니다."
+      />
+      <meta name="twitter:image" content="카드 썸네일 이미지 주소" />
     </>
   );
 }
