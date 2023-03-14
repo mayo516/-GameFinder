9fc7f44bf75be9c803d836a87521ec056c08e6b1
eunha <dmsgk2323@naver.com>
2023-03-14 14:54:55 +0900

    fix: api 요청 안되는 문제 해결 변경된 key값으로 서버에 요청 안가는 문제 해결;

--- src/component/NextBtn.tsx
+++ src/component/NextBtn.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import handleSubmit from "../lib/api_request/chatgpt";
 import {
   RecoilRoot,
   atom,
@@ -9,7 +10,11 @@ import {
 import { btnValueState } from "./Button";
 export default function NextBtn() {
   const btnValue = useRecoilValue(btnValueState);
-  function runGameFinder() {
+  function runGameFinder(e) {
+    const postContent: string =
+      btnValue.join("이고").substr(2) + "인 게임 중에서 추천해줘 ";
+    console.log(postContent);
+    handleSubmit(postContent);
     console.log(btnValue);
   }
 
--- src/lib/api_request/chatgpt.ts
+++ src/lib/api_request/chatgpt.ts
@@ -1,15 +1,19 @@
 import axios from "axios";
 
-const handleSubmit = (postContent) => {
-  e.preventDefault();
+const handleSubmit = (postContent: string) => {
+  // e.preventDefault();
   let prompt = "Hi!";
   // Send a request to the server with the prompt
   axios
-    .post("http://localhost:8080/chat", JSON.stringify({ prompt: "hi!!!" }), {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
+    .post(
+      "http://localhost:8080/chat",
+      JSON.stringify({ prompt: postContent }),
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
     .then((res) => {
       // Update the response state with the server's response
       console.log(res);
